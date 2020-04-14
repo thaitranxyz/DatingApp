c90abd4d9c9e1e4c7c7f5a7b4c01d89e775c4b2e
Thai <tdtrungthai2000@gmail.com>
2020-04-15 00:44:35 +1000

    End of Section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace datingapp.api.Controllers
         {
             _context = context;
         }
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
