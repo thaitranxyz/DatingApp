5be07c9811d2f6ec6d63549e8d5aa3c137c37ec4
Thai <tdtrungthai2000@gmail.com>
2020-06-26 23:16:42 +1000

    End of section 10

--- DatingApp-SPA/src/app/route.ts
+++ DatingApp-SPA/src/app/route.ts
@@ -7,6 +7,9 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolve';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 
 export const AppRoutes: Routes = [
@@ -20,7 +23,9 @@ export const AppRoutes: Routes = [
                 resolve: {users: MemberListResolver}},
             { path: 'members/:id', component: MemberDetailComponent, 
                 resolve: {user: MemberDetailResolver}},
-            { path: 'messages', component: MessagesComponent},
+            {path: 'member/edit', component: MemberEditComponent, 
+                resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges]},    
+            {path: 'messages', component: MessagesComponent},
             { path: 'lists', component: ListsComponent},
         ]
     },
--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -5,6 +5,8 @@ using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System;
 
 namespace DatingApp.API.Controllers
 {
@@ -33,12 +35,28 @@ namespace DatingApp.API.Controllers
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetUser(int id)
-        {
+        { 
             var user = await _repo.GetUser(id);
 
             var userToReturn = _mapper.Map<UserForDetailedDto>(user);
 
             return Ok(userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            
+            var userFromRepo = await _repo.GetUser(id);
+
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if (await _repo.SaveAll())
+                return NoContent();
+
+            throw new Exception($"Updating user {id} failed on save");
         }
     }
 }
