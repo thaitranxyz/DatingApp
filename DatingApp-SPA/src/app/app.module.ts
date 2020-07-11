68a9b5bc92b0d90354c6e53318be18dfc8c817a0
Thai <tdtrungthai2000@gmail.com>
2020-07-12 02:04:17 +1000

    End of section 13

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule } from 'ngx-gallery-9';
 import { FileUploadModule } from 'ng2-file-upload';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker'; 
+import { TimeagoModule } from 'ngx-timeago';
 
 
 import { AppComponent } from "./app.component";
@@ -67,6 +68,7 @@ export function tokenGetter() {
       RouterModule.forRoot(AppRoutes),
       NgxGalleryModule,
       FileUploadModule,
+      TimeagoModule.forRoot(),
       JwtModule.forRoot({
          config: {
             tokenGetter: tokenGetter,
